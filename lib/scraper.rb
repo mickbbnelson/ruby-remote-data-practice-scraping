c84761a20f8dfb033cc13b2055e44f0beb2e1515
mickbbnelson <75447451+mickbbnelson@users.noreply.github.com>
2021-03-30 09:11:14 -0700

    commit tree 40d18400b34d5fa453152c5f691258a625bbc16e

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,3 +2,10 @@ require 'nokogiri'
 require 'open-uri'
 
 html = open("https://flatironschool.com/")
+
+doc = Nokogiri::HTML(open("http://flatironschool.com/"))
+doc.css(".headline-260IBN").text
+
+binding.pry
+
+puts doc.css(".site-header__hero__headline")
